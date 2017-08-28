07830598a33bdb99bf89808ee2c659e43b90b9b8
Eric Abell <ericabell@users.noreply.github.com>
2017-08-28 10:44:17 -0500

    Register page works
    After user registers, they are also in session

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express'),
       path = require('path'),
       favicon = require('serve-favicon'),
       logger = require('morgan'),
+      passport = require('passport'),
+      LocalStrategy = require('passport-local').Strategy,
       cookieParser = require('cookie-parser'),
+      session = require('express-session'),
+      mongoDBStore = require('connect-mongodb-session')(session),
       bodyParser = require('body-parser');
 
 
@@ -19,6 +23,10 @@ mongoose.connect(url,
 
 // ROUTES
 var index = require('./routes/index');
+let auth = require('./routes/auth.js');
+
+// the schema for auth
+let User = require('./models/auth.js');
 
 var app = express();
 
@@ -34,7 +42,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(new LocalStrategy(function(username, password, done) {
+    console.log(`in passport LocalStrategy`);
+    User.findOne({username: username,
+    }, function(err, user) {
+      if(err) {
+        return done(err);
+      }
+
+      if(!user) {
+        return done(null, false, {message: 'incorrect username'});
+      }
+
+      if( !user.authenticate(password) ) {
+        return done( null, false, {message: 'incorrect password'} );
+      }
+
+      return done(null, user);
+    })
+}));
+
+passport.serializeUser(function(user, done) {
+  console.log('in serializeUser');
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  console.log('in deserializeUser');
+  User.findById(id, function(err, user) {
+    done(err, user);
+  })
+});
+
+// set up the express-session store to use MongoDB
+// this allows us to kill the server and still have session data available
+let store = new mongoDBStore(
+  {
+    uri: 'mongodb://localhost:27017/code-snippet-manager-project',
+    collection: 'session-store'
+  }
+);
+
+store.on('error', (e) => {
+  assert.ifError(e);
+  assert.ok(false);
+});
+
+// set up express-session
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 //1 week
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', index);
+app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const passport = require('passport');
+
+let router = express.Router();
+
+let Snippet = require('../models/snippet.js');
+let User = require('../models/auth.js');
+
+
+// USER AUTH ROUTES FOR LOGIN AND REGISTER
+router.get('/login', (req, res) => {
+  res.render('login', {title: 'Code Snip Manager'})
+});
+
+const checkPost = function(req, res, next) {
+  console.log('in checkPost');
+  console.log("body parsing", req.body);
+  next();
+}
+
+router.post('/login', checkPost,
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/failure',
+  })
+);
+
+router.get('/success', (req, res) => {
+  res.send('success!');
+});
+
+router.get('/failure', (req, res) => {
+  res.send('failure!');
+});
+
+
+router.get('/logout', (req, res) => {
+  // TODO: logout passport
+  req.logout();
+  res.redirect('/');
+})
+
+router.get('/register', (req, res) => {
+  res.render('register', {title: 'Code Snip Manager'});
+});
+
+router.post('/register', (req, res) => {
+  User.create({username: req.body.username,
+               password: req.body.password,
+               name: req.body.name
+             },
+    function(err, doc) {
+      if( err ) throw err;
+      console.log(doc);
+      console.log('user created successfully!');
+    });
+  res.redirect('/');
+})
+
+module.exports = router;
