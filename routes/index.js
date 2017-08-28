ad13503785d5b175be4ad3a0b2eda676fb1b5e78
Eric Abell <ericabell@users.noreply.github.com>
2017-08-27 20:46:44 -0500

    Building out skeleton for main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Code Snip Manager' });
 });
 
 module.exports = router;
