0442bd031496a71c21cea04d2890776b9706c768
Eric Abell <ericabell@users.noreply.github.com>
2017-08-28 08:54:16 -0500

    Add create form details and route to add new snippet
    Title, code, and language are fields in the form to add a
    new snippet. After adding, you are redirected to '/'

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,20 @@ router.get('/snip/create', function(req,res,next) {
 router.post('/snip/create', function(req, res, next) {
   // TODO: grab info from form submission and create a new
   //       snippet
-  res.send('snippet created!');
+  let newTitle = req.body.title;
+  let newLanguage = req.body.language;
+  let newCode = req.body.code;
+
+  Snippet.create({title: newTitle,
+                  language: newLanguage,
+                  code: newCode})
+      .then( (doc) => {
+        console.log(`snippet added: ${doc}`);
+        res.redirect('/');
+      })
+      .catch( (err) => {
+        res.send(`Error creating snippet: ${err}`);
+      })
 });
 
 module.exports = router;
